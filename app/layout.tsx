17bebe02082b6066f70bca3885e2b3b5d0729493
shanmukh <shannuthangudu@gmail.com>
2026-04-07 17:22:12 +0530

    Implimented accessing each product data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import { CartContext, CartProvider } from "@/context/CartContext"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <AuthProvider>
-          {children}
+          <CartProvider>
+            {children}
+          </CartProvider>
         </AuthProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -32,10 +32,13 @@ export default function Home() {
   return (
     <div className="flex bg-zinc-50 font-sans h-screen">
       <Sidebar />
-      <div className="mt-[40%] md:mt-[15%] ml-[10%] w-full flex flex-col items-center text-blue-950">
-        <Image src={homePage} alt="image" width={600} height={500}/>
-        <h1 className="text-3xl">Welcome to <span className="font-bold">Grab</span></h1>
-        <p>Pick the items you want...</p>
+      <div className="mt-[40%] md:mt-[10%] ml-[10%] md:w-full flex flex-col items-center text-blue-950">
+        <Image src={homePage} alt="image" width={600} height={500} />
+        <h1 className="text-md md:text-3xl w-[60%]">Grab - <span className="font-bold">Everything You Need, Delivered Fast</span></h1>
+        <p className="w-[60%]">Discover a smarter way to shop with Grab. From everyday essentials to trending products,
+          we bring you quality, affordability, and convenience all in one place.
+          Enjoy seamless browsing, secure payments, and lightning-fast delivery—because you deserve the best, instantly.
+        </p>
       </div>
     </div >
   );
